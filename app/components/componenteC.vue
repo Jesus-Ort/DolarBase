1c6db30ef03d6d6402ea540de9cbcefd618a32a3
Jesus Ortega <xJesus_Nevermor3@hotmail.com>
2025-10-04 12:11:07 -0400

    input numerico v3

--- app/components/componenteC.vue
+++ app/components/componenteC.vue
@@ -21,7 +21,7 @@
           label="Bolívares (Bs)"
           variant="outlined"
           type="text"
-          inputmode="text"
+          inputmode="numeric"
           pattern="[0-9+\-*/. ()]*"
           hide-details
           clearable
@@ -34,7 +34,7 @@
           label="Dólares (USD)"
           variant="outlined"
           type="text"
-          inputmode="text"
+          inputmode="numeric"
           pattern="[0-9+\-*/. ()]*"
           hide-details
           clearable
